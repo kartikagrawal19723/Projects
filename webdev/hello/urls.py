f65cabaff9f0213176f10ac51fcf858bef01796a
Kartik Agrawal <Kartik.Agrawal.19723@gmail.com>
2023-10-20 17:40:09 +0530

    Get frustated

--- webdev/hello/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("kartik", views.kartik, name="kartik"),
-    path("<str:name>", views.greet, name="greet")
-]
